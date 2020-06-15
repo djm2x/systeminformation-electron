6851763d61155d4cc7df6fa26471e80580fff07c
djm2x <dj-m2x@hotmail.com>
2020-06-15 19:14:21 +0100

    change name foler api to node

---
+++ builderAPI copy.ts
@@ -0,0 +1,59 @@
+import * as builder from 'electron-builder';
+
+const productName = 'djapp';
+const version = '1.0.0';
+const ext = 'exe';
+async function main() {
+  const opt: builder.CliOptions = {
+    targets: builder.Platform.WINDOWS.createTarget(),
+    config: {
+      directories: {
+        output: 'build08',
+        // buildResources: `./`,
+        app: './'
+      },
+      buildVersion: '1.0.0',
+      appId: 'com.myCompany.myApp',
+      productName: 'myAppName',
+      copyright: 'Copyright © 2019 myCompany',
+      nsisWeb: {
+        appPackageUrl : 'https://example.com/download/latest',
+        artifactName: `${productName} Web Setup ${version}.${ext}`,
+        allowElevation: true,
+        allowToChangeInstallationDirectory: true,
+        oneClick: false,
+      },
+      win: {
+        artifactName: 'caisse.exe',
+        asar: false,
+        files: [
+          './node_modules/@*',
+          '!./build*',
+          '!./angular*',
+          './main.js',
+          './dist/**/*'
+        ],
+        target: [
+          {
+            target: 'nsis-web',
+            arch: [
+              'ia32',
+              // 'x64'
+            ],
+          }
+        ],
+        // icon: './src/favicon.ico'
+      },
+    },
+  };
+
+  try {
+    const r = await builder.build(opt)
+    console.log('Build OK!', r)
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+//
+main();
